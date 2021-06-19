6082873811144fd18ad9ac440b75fa5d586cfad5
guiprj <guilhermecamargo90@hotmail.com>
2021-06-19 18:52:13 -0300

    atualizações

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,12 @@
 const Sequelize = require('sequelize')
 
+/* const connection = new Sequelize('bancorobotica', 'root', 'dvs2208612', {
+  host: 'localhost',
+  dialect: 'mysql',
+  freezeTableName: true,
+  timezone: "-03:00"
+}) */
+
 const connection = new Sequelize('boncorobotica', 'guidevprof', 'dvs22086122', {
   host: 'mysql742.umbler.com',
   dialect: 'mysql',
