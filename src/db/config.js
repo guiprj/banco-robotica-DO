f89580dcacb5bf7d9e91fc6d75aa8299268bc15b
guiprj <guilhermecamargo90@hotmail.com>
2022-09-11 17:02:06 -0300

    Atualizando

--- src/db/config.js
+++ src/db/config.js
@@ -1,17 +1,1 @@
-const Sequelize = require('sequelize')
-
-/* const connection = new Sequelize('bancorobotica', 'root', 'dvs2208612', {
-  host: 'localhost',
-  dialect: 'mysql',
-  freezeTableName: true,
-  timezone: "-03:00"
-}) */
-
-const connection = new Sequelize('bancorobotica', 'root', 'dvs2208612', {
-  host: 'localhost',
-  dialect: 'mysql',
-  freezeTableName: true,
-  timezone: "-03:00"
-})
-
-module.exports = connection;
+//....
